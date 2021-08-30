572c13f24eaa51066b27c6e3e666eb44be4588b8
Styro457 <raduursache6@gmail.com>
2021-08-30 10:45:03 +0300

    Moved PlayerManager methods here

--- src/main/java/me/arthed/crawling/Crawling.java
+++ src/main/java/me/arthed/crawling/Crawling.java
@@ -2,28 +2,34 @@ package me.arthed.crawling;
 
 import me.arthed.crawling.commands.CrawlingCommand;
 import me.arthed.crawling.impl.WorldGuardImplementation;
+import me.arthed.crawling.listeners.PlayerDamageListener;
 import me.arthed.crawling.listeners.PlayerInteractListener;
 import me.arthed.crawling.listeners.SneakingListener;
 import me.arthed.crawling.listeners.SwimmingToggleListener;
 import me.arthed.crawling.utils.MetricsLite;
 import me.arthed.crawling.utils.UpdateManager;
+import me.arthed.crawling.utils.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.arthed.crawling.config.CrawlingConfig;
-import me.arthed.crawling.players.PlayerManager;
 
+import java.util.HashMap;
 import java.util.Objects;
 
 public class Crawling extends JavaPlugin implements Listener {
 
-    static Crawling plugin;
+    private static Crawling plugin;
     public static Crawling getInstance() {
         return plugin;
     }
+
+    private final HashMap<Player, CrPlayer> playersCrawling = new HashMap<>();
 
     private WorldGuardImplementation worldGuard;
     public WorldGuardImplementation getWorldGuard() {
@@ -33,11 +39,6 @@ public class Crawling extends JavaPlugin implements Listener {
     private CrawlingConfig config;
     public CrawlingConfig getConfig() {
         return this.config;
-    }
-
-    private PlayerManager playerManager;
-    public PlayerManager getPlayerManager() {
-        return this.playerManager;
     }
 
     @Override
@@ -58,12 +59,11 @@ public class Crawling extends JavaPlugin implements Listener {
 
         plugin = this;
         config = new CrawlingConfig("config.yml");
-        playerManager = new PlayerManager();
 
         getServer().getPluginManager().registerEvents(new SneakingListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerInteractListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerDamageListener(), this);
         getServer().getPluginManager().registerEvents(new SwimmingToggleListener(), this);
-        getServer().getPluginManager().registerEvents(this, this);
 
         Objects.requireNonNull(this.getCommand("crawling")).setExecutor(new CrawlingCommand(this));
 
@@ -71,11 +71,7 @@ public class Crawling extends JavaPlugin implements Listener {
 
         if(!config.getBoolean("ignore_updates"))
             new UpdateManager(this).checkUpdates();
-
-
     }
-
-
 
     @Override
     public void onLoad() {
@@ -84,5 +80,34 @@ public class Crawling extends JavaPlugin implements Listener {
             worldGuard = new WorldGuardImplementation(worldGuardPlugin, this);
         }
     }
+
+    @Override
+    public void onDisable() {
+        for(Player playerCrawling : this.playersCrawling.keySet()) {
+            Block blockAbovePlayer = playerCrawling.getLocation().add(0, 1.5, 0).getBlock();
+            playerCrawling.sendBlockChange(blockAbovePlayer.getLocation(), blockAbovePlayer.getBlockData());
+            blockAbovePlayer.getState().update();
+        }
+    }
+
+    public void startCrawling(Player player) {
+        if(!this.playersCrawling.containsKey(player)) {
+            this.playersCrawling.put(player, new CrPlayer(player));
+        }
+    }
+
+    public void stopCrawling(Player player) {
+        this.playersCrawling.remove(player);
+    }
+
+    public boolean isCrawling(Player player) {
+        return player.isSwimming() && this.playersCrawling.containsKey(player);
+    }
+
+    public CrPlayer getPlayerCrawling(Player player) {
+        return this.playersCrawling.get(player);
+    }
+
+
 
 }
