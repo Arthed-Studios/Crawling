c473b68b6ecf994e3d56aa9f75073574a5a2a181
Styro457 <raduursache6@gmail.com>
2021-11-13 11:02:35 +0200

    Fixed bug where right clicking the block above you while crawling would turn it into a barrier

--- src/main/java/me/arthed/crawling/listeners/PlayerInteractListener.java
+++ src/main/java/me/arthed/crawling/listeners/PlayerInteractListener.java
@@ -18,8 +18,9 @@ public class PlayerInteractListener implements Listener {
         Bukkit.getScheduler().runTaskAsynchronously(crawling, () -> {
             if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
                 if (event.getClickedBlock() != null) {
-                    if (event.getClickedBlock().equals(event.getPlayer().getLocation().add(0, 1.5, 0).getBlock())) {
+                    if (event.getClickedBlock().isPassable() && event.getClickedBlock().equals(event.getPlayer().getLocation().add(0, 1.5, 0).getBlock())) {
                         if (crawling.isCrawling(event.getPlayer())) {
+                            event.setCancelled(true);
                             event.getPlayer().sendBlockChange(event.getClickedBlock().getLocation(), Utils.BARRIER_BLOCK_DATA);
                         }
                     }
