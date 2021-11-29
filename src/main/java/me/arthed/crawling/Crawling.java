dc676455b73a320221ebf438fa394592319befdb
Styro457 <raduursache6@gmail.com>
2021-11-29 19:45:43 +0200

    Added NMS modules for versions 1.14, 1.15 and 1.17

--- src/main/java/me/arthed/crawling/Crawling.java
+++ src/main/java/me/arthed/crawling/Crawling.java
@@ -6,11 +6,14 @@ import me.arthed.crawling.listeners.PlayerDeathListener;
 import me.arthed.crawling.listeners.PlayerInteractListener;
 import me.arthed.crawling.listeners.SneakingListener;
 import me.arthed.crawling.listeners.SwimmingToggleListener;
+import me.arthed.crawling.nms.v1_15.NmsPackets_v1_15;
 import me.arthed.crawling.nms.v1_16.NmsPackets_v1_16;
+import me.arthed.crawling.nms.v1_17.NmsPackets_v1_17;
 import me.arthed.crawling.utils.BlockUtils;
 import me.arthed.crawling.utils.MetricsLite;
 import me.arthed.crawling.utils.UpdateManager;
 import me.arthed.nms.NmsPackets;
+import me.arthed.nms.v1_14.NmsPackets_v1_14;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.block.Block;
@@ -51,18 +54,16 @@ public class Crawling extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         //Check if version is compatible and set the proper nms packet manager
-        if(Bukkit.getVersion().contains("1.13"))
-            this.nmsPacketManager = new NmsPackets_v1_16();
-        else if(Bukkit.getVersion().contains("1.14"))
-            this.nmsPacketManager = new NmsPackets_v1_16();
+        if(Bukkit.getVersion().contains("1.14"))
+            this.nmsPacketManager = new NmsPackets_v1_14();
         else if(Bukkit.getVersion().contains("1.15"))
-            this.nmsPacketManager = new NmsPackets_v1_16();
+            this.nmsPacketManager = new NmsPackets_v1_15();
         else if(Bukkit.getVersion().contains("1.16"))
             this.nmsPacketManager = new NmsPackets_v1_16();
         else if(Bukkit.getVersion().contains("1.17"))
-            this.nmsPacketManager = new NmsPackets_v1_16();
+            this.nmsPacketManager = new NmsPackets_v1_17();
         else {
-            Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&cSorry, this plugin works only on 1.13 or higher versions."));
+            Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&cSorry, this plugin works only on 1.14 or higher versions."));
             Bukkit.getPluginManager().disablePlugin(plugin);
             return;
         }
